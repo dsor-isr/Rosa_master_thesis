9dc5d9f617e97d455aca75da9b932421c5e9502a
dgd_rosa <dgd.rosa@gmail.com>
2022-11-30 09:35:48 +0000

    terca feira

--- vision_thesis/detection/src/detection_algorithms/ibvs.py
+++ vision_thesis/detection/src/detection_algorithms/ibvs.py
@@ -7,9 +7,9 @@ import rospy
 import time
 from numpy.linalg import norm
 
-class VisualServoing(self, Kp, Ki, Kd, principalPoint, focalDistance):
+class VisualServoing():
 
-    def __init__(self):
+    def __init__(self, Kp, Ki, Kd, principalPoint, focalDistance, dt):
        #Inicialization
        self.L_matrix = np.matlib.zeros((6, 6)) #for 3 points
        
@@ -18,4 +18,60 @@ class VisualServoing(self, Kp, Ki, Kd, principalPoint, focalDistance):
        self.Ki = Ki
        self.Kd = Kd
 
-       #self.cumulativeError = 
+       self.integralError = np.zeros((6,1))
+       self.lastError = None
+       self.derror_dt = np.zeros((6,1))
+       self.error_meters = None
+       self.mean_error_meters = np.array([0, 0])
+       self.dt = dt
+
+    
+    def generate_L(self, desired_corners, Z):
+        for i in range(0,3):
+            u_img = desired_corners[2*i]
+            v_img = desired_corners[2*i+1]
+            x, y = self.convert_pixels_in_xy(u_img, v_img)
+            self.L_matrix[2*i:2*i+1, :] = np.L_matrix([[-1/Z, 0, x/Z, x*y, -(1+x**2), y], \
+                                                    [0, -1/Z, y/Z, 1+y**2, -x*y, -x]])
+        
+        return self.L_matrix
+
+    def convert_pixels_in_xy(self, u_img, v_img):
+        x = (u_img - principalPoint[0]) / self.focalDistance[0]
+        y = (v_img - principalPoint[1]) / self.focalDistance[1]
+        return x, y
+    
+    def get_error(self, desired_corners, detected_corners):
+        # e = measured - ref
+        error = detected_corners - desired_corners
+        
+        if (self.lastError is not None):
+            # Compute integral Error with Trapezoid Rule
+            self.integralError += (error + self.lastError)*dt/2
+            # Compute derivative Error
+            self.derror_dt = (error - self.lastError)/dt
+
+        self.lastError = error
+
+        return error
+    
+    def compute_desired_pixel_vel(self, desired_corners, detected_corners):
+        desired_pixel_vel = np.zeros((6, 1))
+
+        error = self.get_error(desired_corners, detected_corners)
+        proportional = -np.dot(self.Kp, error)
+        derivative = -np.dot(self.Kd, self.derror_dt)
+        integral = -np.dot(self.Ki, self.integralError)
+
+        desired_pixel_vel = proportional + derivative + integral
+        return desired_pixel_vel
+
+    def compute_new_vel(self, desired_corners, detected_corners, Z):
+        desired_pixel_vel = self.compute_desired_pixel_vel(desired_corners, detected_corners)
+        desired_camera_vel = np.zeros((6,1))
+
+        L = self.generate_L(desired_corners, Z)
+
+        desired_camera_vel = np.dot(np.linalg.inv(L), desired_camera_vel)
+        return desired_camera_vel
+
--- vision_thesis/detection/src/detection_ros/IbvsNode.py
+++ vision_thesis/detection/src/detection_ros/IbvsNode.py
@@ -10,7 +10,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from auv_msgs.msg import NavigationStatus
 from uuv_sensor_ros_plugins_msgs.msg import DVL
-
+import numpy as np
 class DetectionNode():
     def __init__(self):
         """
@@ -24,7 +24,7 @@ class DetectionNode():
 
         self.loadParams()
         
-        self.visual_servoing = VisualServoing()
+        self.visual_servoing = VisualServoing(self.Kp, self.Ki, self.Kd, self.principalPoint, self.focalDistance)
         
         
         self.initializeSubscribers()
@@ -38,7 +38,17 @@ class DetectionNode():
     def loadParams(self):
         self.node_frequency = rospy.get_param('~node_frequency', 10)
 
+        self.principalPoint = rospy.get_param('~principalPoint')
+        self.focalDistance = rospy.get_param('~focalDistance')
+        Kp = rospy.get_param('~Kp')
+        Ki = rospy.get_param('~Ki')
+        Kd = rospy.get_param('~Kd')
+        self.Vel_Sat = rospy.get_param('~Velocity_Saturation')
         self.vehicle = rospy.get_param('~Vehicle')
+
+        self.Kp = np.diag(Kp)
+        self.Ki = np.diag(Ki)
+        self.Kd = np.diag(Kd)
     
 
     """
